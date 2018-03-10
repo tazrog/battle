d6c0a4eb14b0c93f8cd84d38cb3cdc34d04ad9b9
tazrog <roger.scharm@outlook.com>
2018-03-10 09:44:16 -0800

    Updated player HP

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ magic =[{"name": "Fire", "cost": 10, "dmg": 90},
         {"name": "Blizzard", "cost": 10, "dmg": 60},]
 
 
-player = person(680, 65, 60, 85, magic)
+player = person(680, 65, 60, 95, magic)
 enemy = person(1000, 65, 45, 35, magic)
 
 running =True
